7e2ba50ed02071c391de09e422b844adf0964c8a
luigidamico100 <luigidamico.1095@gmail.com>
2022-01-11 19:15:56 +0100

    Update results_analysis

--- results_analysis/results_analysis.py
+++ results_analysis/results_analysis.py
@@ -12,26 +12,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 from sklearn.metrics import accuracy_score, confusion_matrix
-
 on_cuda = torch.cuda.is_available()
 
-work_dir_path = '/home/luigi.damico/Video-Swin-Transformer/work_dirs/experiment_2/' if on_cuda else '/Users/luigidamico/Documents/GitHub/Video-Swin-Transformer/work_dirs/experiment_2/'
-dataset_path = '/home/luigi.damico/ICPR_datasets/ICPR_rawframes_numframes2/' if on_cuda else '/Volumes/SD Card/Thesis/ICPR/Dataset_rawframe/ICPR_rawframes_numframes2/'
+EXPERIMENT_NAME=22
+
+work_dir_path = '/home/luigi.damico/Video-Swin-Transformer/work_dirs/experiment_'+str(EXPERIMENT_NAME)+'/' if on_cuda else '/Users/luigidamico/Documents/GitHub/Video-Swin-Transformer/work_dirs/experiment_'+str(EXPERIMENT_NAME)+'/'
+# dataset_path = '/home/luigi.damico/ICPR_datasets/ICPR_rawframes_numframes2/' if on_cuda else '/Volumes/SD Card/Thesis/ICPR/Dataset_rawframe/ICPR_rawframes_numframes2/'
+
+def plot_training(training_stats_df, testfold):
+    fig, axs = plt.subplots(2,1)
+    training_stats_df.loc[testfold][['Loss_train','Loss_val','Loss_test']].plot(ax=axs[0])
+    training_stats_df.loc[testfold][['Accuracy_train','Accuracy_val','Accuracy_test']].plot(ax=axs[1])
+    for ax in axs: ax.grid()
+    fig.suptitle(f'test fold: {testfold}')
+    
+    
+#%% reading files
+training_stats_df = pd.read_csv(work_dir_path + 'results/results_stats.csv', index_col=[0,1])
+
+with open(work_dir_path + 'results/best_epoch_dict.pkl', 'rb') as handle:   best_epochs_dict = pickle.load(handle)
 
 
-#%%
-dataframe_stats = pd.read_csv(work_dir_path + 'results/results_stats.csv', index_col=[0,1])
-
-with open(work_dir_path + 'results/best_epoch_dict.pkl', 'rb') as handle:
-    best_epochs_dict = pickle.load(handle)
+#%% main
+for testfold in range(0, 10): plot_training(training_stats_df, testfold=testfold)
 
 
-testfold = 2
-fig, axs = plt.subplots(2,1)
-dataframe_stats.loc[testfold][['Loss_train','Loss_val','Loss_test']].plot(ax=axs[0])
-dataframe_stats.loc[testfold][['Accuracy_train','Accuracy_val','Accuracy_test']].plot(ax=axs[1])
 
 
-test_informations = pd.read_csv(work_dir_path + 'results/results_information.csv', index_col=0)
 
-accuracy_score(test_informations['true_class'], test_information['predicted_class'])
+
