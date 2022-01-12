53543fd33e5435540838a5e3d79adb4cf3fd726f
luigidamico100 <luigidamico.1095@gmail.com>
2022-01-12 01:49:42 +0100

    Update train_different_folds_ICPR.sh

--- tools/train_different_folds_ICPR.sh
+++ tools/train_different_folds_ICPR.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 ### To edit ###
-NUM_EPOCHS=20
+NUM_EPOCHS=18
 NUM_FOLDS=10
 NUM_FRAMES_PER_VIDEO=2
-EXPERIMENT_NAME=trial_1
-KEEP_ONLY_BEST=1
+EXPERIMENT_NAME=27
+KEEP_ONLY_BEST=0
 VIDEOS_PER_GPU_TRAIN=2
 ###############
 
-DATASET_DIR_IN=/home/luigi.damico/ICPR_datasets/ICPR_rawframes_numframes${NUM_FRAMES_PER_VIDEO}/
+DATASET_DIR_IN=/home/luigi.damico/ICPR_datasets/ICPR_rawframes_allFrames/
 WORK_DIR=/home/luigi.damico/Video-Swin-Transformer/work_dirs/experiment_${EXPERIMENT_NAME}/
 TRAINING_DIR=${WORK_DIR}training/
 PREDICTION_DIR_OUT=${WORK_DIR}predictions/
@@ -21,14 +21,16 @@ echo ---- Starting training phase
 for FOLD_TEST in $(seq 0 $((NUM_FOLDS-1)))
 do
   python tools/train.py configs/recognition/swin/swin_tiny_patch244_window877_ICPR_1k_rawframe.py  \
-  --cfg-options model.backbone.pretrained=backbones/swin_tiny_patch4_window7_224.pth \
-  total_epochs=${NUM_EPOCHS} \
+  --cfg-options total_epochs=${NUM_EPOCHS} \
   work_dir=${TRAINING_DIR}testfold_${FOLD_TEST} \
   data.train.data_prefix=${DATASET_DIR_IN}foldtest_${FOLD_TEST}/rawframes_train \
   data.train.ann_file=${DATASET_DIR_IN}foldtest_${FOLD_TEST}/ICPR_train_list_rawframes.txt \
   data.videos_per_gpu=${VIDEOS_PER_GPU_TRAIN}
+  #data.train.pipeline.0.clip_len=${NUM_FRAMES_PER_VIDEO}
 done
 
+
+echo ---- Starting evaluation phase
 for PHASE in $PHASES
 do
   for FOLD_TEST in $(seq 0 $((NUM_FOLDS-1)))
@@ -42,13 +44,14 @@ do
       --out ${PREDICTION_DIR_OUT}${PHASE}/testfold_${FOLD_TEST}/epoch_${EPOCH}.pkl \
       --cfg-options data.test.data_prefix=${DATASET_DIR_IN}foldtest_${FOLD_TEST}/rawframes_${PHASE} \
       data.test.ann_file=${DATASET_DIR_IN}foldtest_${FOLD_TEST}/ICPR_${PHASE}_list_rawframes.txt
+      #data.val.pipeline.0.clip_len=${NUM_FRAMES_PER_VIDEO}
+      #data.test.pipeline.0.clip_len=${NUM_FRAMES_PER_VIDEO}
     done
   done
 done
 
 
 echo ---- Generating results csv
-
 python results_analysis/generate_results_csv.py \
 ${WORK_DIR} \
 ${DATASET_DIR_IN} \
